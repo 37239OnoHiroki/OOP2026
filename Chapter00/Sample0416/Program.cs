c3da94c61544b429a43624fd9a60b5e7e6521c64
小野 紘輝 <oibc37239@ojs.ac.jp>
2026-04-16 11:51:05 +0900

    繰り返し処理をfor文からwhile文に変更した

--- Chapter00/Sample0416/Program.cs
+++ Chapter00/Sample0416/Program.cs
@@ -4,9 +4,12 @@
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 10; i++)
+
+            int i = 0;
+            while ( i < 10)
             {
                 Console.WriteLine("Hello, World!");
+                i++;
             }
         }
     }
