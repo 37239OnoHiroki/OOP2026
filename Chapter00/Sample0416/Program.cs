63aa67678da88583b28d9d9408f2a27f58549fa1
小野 紘輝 <oibc37239@ojs.ac.jp>
2026-04-20 11:42:59 +0900

    p30コード2-3まで入力

--- Chapter00/Sample0416/Program.cs
+++ Chapter00/Sample0416/Program.cs
@@ -11,7 +11,7 @@
                 Console.WriteLine("Hello, World!");
                 i++;
             }
-            Console.WriteLine(i + "回繰り返されました");
+            Console.WriteLine(i + "回繰り返された");
         }
     }
 }
---
+++ Chapter02/距離換算プログラム/DistanceConverter/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace DistanceConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length >= 1 && args[0] == "-tom")
+            {
+
+
+                //フィートからメートルへの対応表を出力
+                for (int feet = 1; feet <= 10; feet++)
+                {
+                    double meter = FeetToMeter(feet);
+                    Console.WriteLine($"{feet}ft ={meter:0.0000}m");
+                }
+
+            }
+            else
+            {
+
+                //メートル法からフィートへの対応表を出力
+                for (int meter = 1; meter <= 10; meter++)
+                {
+                    double feet = FeetToMeter(meter);
+                    Console.WriteLine($"{meter}m ={feet:0.0000}ft");
+                }
+
+            }
+
+
+            static double FeetToMeter(int feet)
+            {
+                //feet*0.3048
+                return feet * 0.3048;
+            }
+
+            static double MeterToFeet(int feet)
+            {
+                return feet / 0.3048;
+            }
+
+        }
+    }
+}
