fccb22bae188f7d5259df48c9c9459a9b4836c0b
小野 紘輝 <oibc37239@ojs.ac.jp>
2026-05-11 10:25:57 +0900

    2.1.3まで完了

---
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var songs = new Song[] {
+               new Song("Let it be", "The Beatles", 243),
+               new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293),
+               new Song("Close To You", "Carpenters", 276),
+               new Song("Honesty", "Billy Joel", 231),
+               new Song("I Will Always Love You", "Whitney Houston", 273),
+             };
+            PrintSongs(songs);
+        }
+        private static void PrintSongs(Song[] songs) {
+
+        }
+    }
+}
