5c05058dc5b2b5ac4674a09fb58f16bde2b785f4
小野 紘輝 <oibc37239@ojs.ac.jp>
2026-05-13 11:16:57 +0900

    問題2.2完成

--- Chapter02/演習問題/Exercise02/Program.cs
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -1,7 +1,15 @@
 ﻿namespace Exercise02 {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+            PrintInchToMeterList(1, 10);
+        }
+
+        //インチからメートルに変換
+        public static void PrintInchToMeterList(int start, int end) {
+            for (int inch = start; inch <= end; inch++) {
+                double meter = InchConverter.ToMeter(inch);
+                Console.WriteLine($"{inch}inch ={meter:0.0000}m");
+            }
         }
     }
 }
