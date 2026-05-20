dd604950febb61cafeb63a929a0b87ce589e2fd5
小野 紘輝 <oibc37239@ojs.ac.jp>
2026-05-20 10:36:05 +0900

    p71まで

--- Chapter03/ラムダ式とLINQ/Section01/Program.cs
+++ Chapter03/ラムダ式とLINQ/Section01/Program.cs
@@ -2,22 +2,42 @@
     internal class Program {
 
         static void Main(string[] args) {
-            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+            var cities = new List<string> {//citiesコレクション
+                "Tokyo",
+                "New Delhi",
+                "Bangkok",
+                "London",
+                "Paris",
+                "Berlin",
+                "Canberra",
+                "Hong Kong",
+            };
+
+            //var exists = cities.Exists (s => s[0] == 'B');
+            //Console.WriteLine(exists);
+
+            //var name = cities.Find(s => s.Length == 6);
+            //Console.WriteLine(name);
+
+            ///int index = cities.FindIndex(s => s == "Berlin");
+            //Console.WriteLine(index);
+
+            //cities.ForEach(s => Console.WriteLine(s));
+
+            //cities.ForEach(Console.WriteLine);
+
+            //var exist = cities.FindAll(s => s[0] == 'B');
+            //exist.ForEach(s => Console.WriteLine(s));
 
 
-            var count = Count(numbers, n =>n%4 == 0 || n % 5 == 0);
-            Console.WriteLine(count);
-        }
 
-        static int Count (int[] numbers,Predicate<int> judge) {
-            
-            var count = 0;
-            foreach (var n in numbers) {
-                if (judge(n) == true) {
-                    count++;
-                }
-            }
-            return count;
+
+            //以下の条件を満たすものが存在しているか調べる
+            //大文字が6文字以上
+            //oを含む
+            //最後がｎ
+            var exis = cities.Exists(s => 6 <=s.Length && s.Contains('o') && s.EndsWith('n'));
+            Console.WriteLine(exis);
         }
     }
 }
