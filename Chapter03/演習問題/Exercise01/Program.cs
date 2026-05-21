adf63595339d40ece397ef9768f743683bd61786
小野 紘輝 <oibc37239@ojs.ac.jp>
2026-05-21 09:49:33 +0900

    問題3.1.4完成

--- Chapter03/演習問題/Exercise01/Program.cs
+++ Chapter03/演習問題/Exercise01/Program.cs
@@ -21,22 +21,32 @@ namespace Exercise01 {
         }
 
         private static void Exercise1(List<int> numbers) {
-            var exis = numbers.Exists(s => s %8 ==0 || s %9 ==0);
+            Console.WriteLine("---3.1.1----");
+            var exis = numbers.Exists(num => num %8 ==0 || num %9 ==0);
             if (exis == true) {
                 Console.WriteLine("存在しています");
             }
         }
 
         private static void Exercise2(List<int> numbers) {
-            
-            
+            Console.WriteLine("---3.1.2----");
+            numbers.ForEach(num => Console.WriteLine( num /2.0));
         }
 
         private static void Exercise3(List<int> numbers) {
-            
+            Console.WriteLine("---3.1.3----");
+            var query = numbers.Where(s => s >= 50).ToList();
+            foreach (var item in query) {
+                Console.WriteLine(item);
+            }
         }
 
         private static void Exercise4(List<int> numbers) {
+            Console.WriteLine("---3.1.4----");
+            var query = numbers.Select(s => s *2).ToList();
+            foreach (int num in query) {
+                Console.WriteLine(num);
+            }
 
         }
     }
