f31a401cdce388730c03612489973b12b6e44dbc
小野 紘輝 <oibc37239@ojs.ac.jp>
2026-05-27 11:52:15 +0900

    4.1.3　模範解答

--- Chapter04/演習問題/Exercise01/Program.cs
+++ Chapter04/演習問題/Exercise01/Program.cs
@@ -62,9 +62,7 @@ namespace Exercise01 {
         }
 
         private static void Exercise3(List<string> langs) {
-            string? lang = null;
-            var query = langs.Find(s => s.Length == 10)??(lang = "unknown");
-            lang =  query;
+            var lang = langs.Find(s => s.Length == 10)??"unknown";
             Console.WriteLine(lang);
         }
     }
--- Chapter04/演習問題/Exercise02/Program.cs
+++ Chapter04/演習問題/Exercise02/Program.cs
@@ -13,12 +13,32 @@ namespace Exercise02 {
         private static void Exercise1() {
             //if-else文を使用
             var number = Console.ReadLine();
+            if (int.TryParse(number, out int result)) {
 
+                if (result < 0) {
+                    Console.WriteLine(result);
+                } else if (0 < result && result < 100) {
+                    Console.WriteLine(result * 2);
+                } else if (100 < result && result < 500) {
+                    Console.WriteLine(result * 5);
+                } else if (500 < result) {
+                    Console.WriteLine(result);
+                }
+            } else Console.WriteLine("入力値に誤りがあります");
         }
 
         private static void Exercise2() {
             //switch文を使用
+            var number = Console.ReadLine();
+            if (int.TryParse(number, out int result)) {
 
+                switch (result) {
+                    case (result < 0):
+                        break;
+
+                }
+
+            } else Console.WriteLine("入力値に誤りがあります");
         }
 
         private static void Exercise3() {
