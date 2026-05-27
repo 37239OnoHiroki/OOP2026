d62b731d6079a8fee0f2c66938ea12003baabcbb
小野 紘輝 <oibc37239@ojs.ac.jp>
2026-05-27 09:58:29 +0900

    4.1.1完成

---
+++ Chapter04/演習問題/Exercise01/Program.cs
@@ -0,0 +1,65 @@
+﻿
+using System.Globalization;
+using System.Linq;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            List<string> langs = [
+               "C#", "Java", "Ruby", "PHP", "Python", "TypeScript",
+                "JavaScript", "Swift", "Go",
+            ];
+
+            Exercise1(langs);
+            Console.WriteLine("---");
+            Exercise2(langs);
+            Console.WriteLine("---");
+            Exercise3(langs);
+        }
+
+        private static void Exercise1(List<string> langs) {
+            //foreach文
+            Console.WriteLine("foreach文で出力");
+            foreach (var item in langs){
+                if (item.Contains('S')) {
+                    Console.WriteLine(item);
+                }
+            }
+
+
+
+            //for文
+            Console.WriteLine("\nfor文で出力");
+            for (int i = 0; i < langs.Count; i++) {
+                if (langs[i].Contains('S')) {
+                    Console.WriteLine(langs[i]);
+                }
+            }
+
+
+
+            //while文
+            Console.WriteLine("\nwhile文で出力");
+            int j = 0;
+            while (j<langs.Count) {
+                if (langs[j].Contains('S')) {
+                    Console.WriteLine(langs[j]);
+                }
+                j++;
+            }
+            
+
+
+
+
+        }
+
+        private static void Exercise2(List<string> langs) {
+
+        }
+
+        private static void Exercise3(List<string> langs) {
+
+        }
+    }
+}
