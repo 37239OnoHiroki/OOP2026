2cbe89ee475ef5b2088f28f06fdd1320363cb388
小野 紘輝 <oibc37239@ojs.ac.jp>
2026-06-01 10:44:00 +0900

    5,1,2完成

--- Chapter05/演習問題/Exercise01/YearMonth.cs
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -14,5 +14,13 @@ namespace Exercise01 {
             year = Year;
             month = Month;
         }
+        //5.1.2(p116参照）
+        //設定されている西暦が21世紀か判断する
+        //Yearが2001～2100の間ならtrue
+        public bool Is21Century { get { return (2001<=Year&&Year <= 2100); } }
+
+        //public YearMonth AddOneMonth(int year ,int month ) {
+            
+        //}
     }
 }
