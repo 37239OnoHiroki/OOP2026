db228b4e7693e4d61483ab26a8bce1658b0fd8fc
小野 紘輝 <oibc37239@ojs.ac.jp>
2026-06-10 09:54:32 +0900

    6.1 完成

---
+++ chapter06/演習問題/Exercise01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var str1 = Console.ReadLine();
+            var str2 = Console.ReadLine();
+
+            var cultureInfo = new CultureInfo("ja-JP");
+
+            if (String.Compare(str1, str2, cultureInfo,
+                CompareOptions.IgnoreWidth | CompareOptions.IgnoreKanaType| CompareOptions.IgnoreCase) == 0) {
+                Console.WriteLine("等しい");
+            } else {
+                Console.WriteLine("等しくない");
+            }
+
+            
+
+        }
+    }
+}
