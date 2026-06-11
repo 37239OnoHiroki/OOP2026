def7c370c05be3fe9decc4501bd5837851b43a5b
小野 紘輝 <oibc37239@ojs.ac.jp>
2026-06-11 11:03:50 +0900

    6.3.6 模範解答

--- chapter06/演習問題/Exercise03/Program.cs
+++ chapter06/演習問題/Exercise03/Program.cs
@@ -61,17 +61,14 @@ namespace Exercise03 {
             var str = text.ToLower().Replace(" ","");
 
             //辞書（ディクショナリ）を使った集計
-            //var alphDicCount = Enumerable.Range('a', 26).
-            //    ToDictionary(num => ((char)num).ToString(), num => 0);
+            var alphDicCount = Enumerable.Range('a', 26).
+                ToDictionary(num => ((char)num), num => 0);
 
-            var dict = new SortedDictionary<char, int>();
+            //var dict = new SortedDictionary<char, int>();
             foreach (var c in str) {
-                if (dict.ContainsKey(c))
-                    dict[c]++;//登録されている場合
-                else
-                    dict[c] = 1; //未登録の場合
+                alphDicCount[c]++;
             }
-            foreach(var word in dict) {
+            foreach(var word in alphDicCount) {
                 Console.WriteLine(word.Key + ":" + word.Value);
             }
 
