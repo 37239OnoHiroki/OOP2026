1db440ad76af1b7e0523b96359eb5121cc7a81d8
小野 紘輝 <oibc37239@ojs.ac.jp>
2026-06-29 15:49:35 +0900

    第８章演習問題

---
+++ Chapter08/演習問題/Exercise01/Program.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var text = "Cozy lummox gives smart squid who asks for job pen";
+
+            Exercise1(text);
+            Console.WriteLine();
+            Exercise2(text);
+            Console.WriteLine();
+        }
+
+        private static void Exercise1(string text) {
+            var 
+        }
+
+        private static void Exercise2(string text) {
+
+        }
+    }
+}
