5421d4d271d3b2b158fd37055e0db3083fce3a2e
小野 紘輝 <oibc37239@ojs.ac.jp>
2026-07-09 15:48:54 +0900

    stopwatch クラス

---
+++ Chapter09/演習問題/Exercise03/Form1.cs
@@ -0,0 +1,54 @@
+using System.Diagnostics;
+using System.Runtime.InteropServices.Marshalling;
+
+namespace Exercise03 {
+    public partial class Form1 : Form {
+        Stopwatch sw = new Stopwatch();
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btStart_Click(object sender, EventArgs e) {
+            sw.Start();
+            timer1.Start();
+        }
+
+        private void btStop_Click(object sender, EventArgs e) {
+            sw.Stop();
+            timer1.Stop();
+            lbTimeDisp.Text = $"{sw.Elapsed}•b";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) {
+            lbTimeDisp.Text = $"{sw.Elapsed}•b";
+        }
+
+        private void btReset_Click(object sender, EventArgs e) {
+            sw.Reset();
+            lbTimeDisp.Text = $"{sw.Elapsed}";
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e) {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            listBox1.Items 
+        }
+    }
+}
+
+//internal class TimeWatch {
+//    private DateTime _time;
+
+
+//    public void Start() {
+//        _time= DateTime.Now;
+        
+//    }
+
+//    public TimeSpan Stop() {
+//        return DateTime.Now-_time;
+//    }
+//}
