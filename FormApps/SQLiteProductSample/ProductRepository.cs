bf0cbd40f535e7e31f18ab0f316e3a1cf5fda64f
小野 紘輝 <oibc37239@ojs.ac.jp>
2026-08-31 15:31:12 +0900

    SELECT文の実績

--- FormApps/SQLiteProductSample/ProductRepository.cs
+++ FormApps/SQLiteProductSample/ProductRepository.cs
@@ -2,7 +2,38 @@ using Microsoft.Data.Sqlite;
 
 namespace SQLiteProductSample;
 
+//Productテーブルに対するDB操作をまとめたクラス
+//CRUD（CREATE、READ、Update、）
 public class ProductRepository
 {
-    
+    //全商品を取得するRead（SELECT）を担当する
+    public List<Product> GetAll() {
+
+        var products = new List<Product>();
+
+        using var connection = Database.GetConnection();
+        connection.Open();
+
+        //SQLを実行するためのコマンドオブジェクトを作る
+        using var command = connection.CreateCommand();
+
+        //Productsテーブルを作るSQL
+        command.CommandText =
+            """
+            SELECT Id, Name, Price
+            FROM Products
+            ORDER BY Id;
+            """;
+        //SELECTを実行し、複数行の検索結果を読み取る
+        using var reader = command.ExecuteReader();
+
+        while (reader.Read()) {
+            products.Add(new Product {
+                Id = reader.GetInt32(0), //0列目：Id
+                Name = reader.GetString(1), //１列目: Name
+                Price = reader.GetInt32(2) //2列目：　Price
+            });
+        }
+        return products;
+    }
 }
